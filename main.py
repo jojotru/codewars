aa2858ef72ca9314c753fcd00e06e0bee467d6fe
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-08-07 22:28:35 -0400

    Challenge 8/7

--- main.py
+++ main.py
@@ -20,3 +20,14 @@ import math
 
 def find_next_square(sq):
     return (math.sqrt(sq) + 1) ** 2 if (math.sqrt(sq)).is_integer() else -1
+
+
+# Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+# Examples input/output:
+
+# XO("ooxx") => true
+# XO("xooxx") => false
+# XO("ooxXm") => true
+# XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+# XO("zzoo") => false
