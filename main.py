07cb6b9a5d8bf4fc64aa267cb1be137bf68680a5
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-08-10 23:03:48 -0400

    Challenge 8/10

--- main.py
+++ main.py
@@ -64,3 +64,13 @@ def remove_smallest(numbers):
 
 # The reason this also works for strings is that in Python, Strings are arrays of bytes representing Unicode characters. !!!!!!!!!!!
 
+#_______________
+# Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+# Examples:
+
+# solution('abc', 'bc') # returns true
+# solution('abc', 'd') # returns false
+
+def solution(string, ending):
+    return string.endswith(ending)
