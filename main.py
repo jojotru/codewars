21647f7b27d6352658333f182790c751c8de96ec
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-08-14 21:36:06 -0400

    Challenge 8/14

--- main.py
+++ main.py
@@ -138,3 +138,41 @@ def find_short(s):
 
 def friend(x):
     return[n for n in x if len(n) == 4]
+
+
+# DropCaps means that the first letter of the starting word of the paragraph should be in caps and the remaining lowercase, just like you see in the newspaper.
+
+# But for a change, let"s do that for each and every word of the given String. Your task is to capitalize every word that has length greater than 2, leaving smaller words as they are.
+
+# *should work also on Leading and Trailing Spaces and caps.
+
+# "apple"            => "Apple"
+# "apple of banana"  => "Apple of Banana"
+# "one   space"      => "One   Space"
+# "   space WALK   " => "   Space Walk   " 
+
+
+def drop_cap(str_):
+    return ' '.join( w.capitalize() if len(w) > 2 else w for w in str_.split(' ') )
+
+
+
+# Given a list of integers, determine whether the sum of its elements is odd or even.
+
+# Give your answer as a string matching "odd" or "even".
+
+# If the input array is empty consider it as: [0] (array with a zero).
+
+# Examples:
+# Input: [0]
+# Output: "even"
+
+# Input: [0, 1, 4]
+# Output: "odd"
+
+# Input: [0, -1, -5]
+# Output: "even"
+
+
+def oddOrEven(arr):
+    return 'even' if sum(arr) % 2 == 0 else 'odd'
