68cae7e2cd2303b3c7e879a76630a01a39b96add
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-08-15 22:47:32 -0400

    Challenge 8/15

--- main.py
+++ main.py
@@ -176,3 +176,22 @@ def drop_cap(str_):
 
 def oddOrEven(arr):
     return 'even' if sum(arr) % 2 == 0 else 'odd'
+
+
+# In this kata, you are asked to square every digit of a number and concatenate them.
+
+# For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+# Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+def square_digits(num):
+    num = str(num)
+    ans = ''
+    for i in num:
+        ans += str(int(i)**2)
+    return int(ans)
+
+#or
+
+def square_digits(num):
+    return int(''.join(str(int(d)**2) for d in str(num)))
