4ccb21473e47034bec22742baaf7bb48925b21b3
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-09-17 23:46:22 -0400

    challenge 9/17

--- main.py
+++ main.py
@@ -285,11 +285,13 @@ def create_phone_number(n):
 
 #-----------------
 
-In this example you have to validate if a user input string is alphanumeric. The given string is not nil/null/NULL/None, so you don't have to check that.
+# In this example you have to validate if a user input string is alphanumeric. The given string is not nil/null/NULL/None, so you don't have to check that.
 
-The string has the following conditions to be alphanumeric:
+# The string has the following conditions to be alphanumeric:
 
-At least one character ("" is not valid)
-Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
-No whitespaces / underscore.
+# At least one character ("" is not valid)
+# Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+# No whitespaces / underscore.
 
+def alphanumeric(string):
+    return string.isalnum()
