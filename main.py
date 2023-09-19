fb9b377733d09c2b0b193832c40a3c255d24d6e8
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-09-18 23:32:51 -0400

    Challenge 9/18

--- main.py
+++ main.py
@@ -295,3 +295,13 @@ def create_phone_number(n):
 
 def alphanumeric(string):
     return string.isalnum()
+
+#______________
+
+# The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+# What if the string is empty? Then the result should be empty object literal, {}.
+
+def count(s):
+    # The function code should be here
+    return {c:s.count(c) for c in s}
