36cd4b46cd597702f64a096a89338757c6fad3d7
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-09-19 23:52:38 -0400

    Challenge 9/19

--- main.py
+++ main.py
@@ -305,3 +305,26 @@ def alphanumeric(string):
 def count(s):
     # The function code should be here
     return {c:s.count(c) for c in s}
+
+# Your task, is to create N×N multiplication table, of size provided in parameter.
+
+# For example, when given size is 3:
+
+# 1 2 3
+# 2 4 6
+# 3 6 9
+# For the given example, the return value should be:
+
+# [[1,2,3],[2,4,6],[3,6,9]]
+def multiplication_table(size):
+
+    result = []
+    for i in range (1,size+1):
+        result.append([(j * i) for j in range(1, size+1)])
+    return result
+
+#or
+
+def multiplicationTable(size):
+    return [[j*i for j in range(1, size+1)] for i in range(1, size+1)]
+
