c9377a4bfc89ccebc981e9c04d1994b2432a1723
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-09-21 23:47:30 -0400

    Challenge 9/21

--- main.py
+++ main.py
@@ -306,6 +306,7 @@ def count(s):
     # The function code should be here
     return {c:s.count(c) for c in s}
 
+#_____________________
 # Your task, is to create N×N multiplication table, of size provided in parameter.
 
 # For example, when given size is 3:
@@ -328,6 +329,7 @@ def multiplication_table(size):
 def multiplicationTable(size):
     return [[j*i for j in range(1, size+1)] for i in range(1, size+1)]
 
+#_____________________
 
 # In this kata you are required to, given a string, replace every letter with its position in the alphabet.
 
@@ -341,3 +343,40 @@ def multiplicationTable(size):
 
 def alphabet_position(s):
   return " ".join(str(ord(c)-ord("a")+1) for c in s.lower() if c.isalpha())
+
+#_____________________
+
+# Encrypt this!
+
+# You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+# Your message is a string containing space separated words.
+# You need to encrypt each word in the message using the following rules:
+# The first letter must be converted to its ASCII code.
+# The second letter must be switched with the last letter
+# Keepin' it simple: There are no special characters in the input.
+# Examples:
+# encrypt_this("Hello") == "72olle"
+# encrypt_this("good") == "103doo"
+# encrypt_this("hello world") == "104olle 119drlo"
+
+def encrypt_this(text):
+    result = []
+    
+    for word in text.split():
+        # turn word into a list
+        word = list(word)
+        
+        # replace first letter with ascii code
+        word[0] = str(ord(word[0]))
+        
+        # switch 2nd and last letters
+        if len(word) > 2:
+            word[1], word[-1] = word[-1], word[1]
+        
+        # add to results
+        result.append(''.join(word))
+    
+    return ' '.join(result)
+
+#_____________________
