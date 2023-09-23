e0cce7a0fc369d865c50c5380185c48753fe1da3
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-09-22 23:21:53 -0400

    challenge 9/22

--- main.py
+++ main.py
@@ -346,37 +346,39 @@ def alphabet_position(s):
 
 #_____________________
 
-# Encrypt this!
+Encrypt this!
 
-# You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
 
-# Your message is a string containing space separated words.
-# You need to encrypt each word in the message using the following rules:
-# The first letter must be converted to its ASCII code.
-# The second letter must be switched with the last letter
-# Keepin' it simple: There are no special characters in the input.
-# Examples:
-# encrypt_this("Hello") == "72olle"
-# encrypt_this("good") == "103doo"
-# encrypt_this("hello world") == "104olle 119drlo"
+Your message is a string containing space separated words.
+You need to encrypt each word in the message using the following rules:
+The first letter must be converted to its ASCII code.
+The second letter must be switched with the last letter
+Keepin' it simple: There are no special characters in the input.
+Examples:
+encrypt_this("Hello") == "72olle"
+encrypt_this("good") == "103doo"
+encrypt_this("hello world") == "104olle 119drlo"
 
-def encrypt_this(text):
-    result = []
+
+
+
+
+# def encrypt_this(text):
+#     result = []
     
-    for word in text.split():
-        # turn word into a list
-        word = list(word)
+#     for word in text.split():
+#         # turn word into a list
+#         word = list(word)
         
-        # replace first letter with ascii code
-        word[0] = str(ord(word[0]))
+#         # replace first letter with ascii code
+#         word[0] = str(ord(word[0]))
         
-        # switch 2nd and last letters
-        if len(word) > 2:
-            word[1], word[-1] = word[-1], word[1]
+#         # switch 2nd and last letters
+#         if len(word) > 2:
+#             word[1], word[-1] = word[-1], word[1]
         
-        # add to results
-        result.append(''.join(word))
+#         # add to results
+#         result.append(''.join(word))
     
-    return ' '.join(result)
-
-#_____________________
+#     return ' '.join(result)
