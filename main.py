68baa11b0861315fc239772989f47144c43b4d95
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-09-27 23:22:14 -0400

    Challenge 9/27

--- main.py
+++ main.py
@@ -415,3 +415,22 @@ def check_alive(health):
         return False
     else:
         return True
+    
+
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+# Note: if there is nothing to sum, the sum is default to 0.
+
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
+
+#or
+
+def positive_sum(arr):
+    sum = 0
+    for x in arr:
+        if x > 0:
+            sum = sum + x
+    return sum
