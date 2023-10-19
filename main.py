ed96eebdba0fc9acb5f7794b984c64395316ab2b
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-10-19 12:46:02 -0400

    Challenge 10/19.2

--- main.py
+++ main.py
@@ -728,3 +728,34 @@ def to_day_of_year(date):
 
 def greet(name):
     return "Hello, " + name + " how are you doing today?"
+
+# Two players - "black" and "white" are playing a game. The game consists of several rounds. If a player wins in a round, he is to move again during the next round. If a player loses a round, it's the other player who moves on the next round. Given whose turn it was on the previous round and whether he won, determine whose turn it is on the next round.
+
+# Input/Output
+# [input] string lastPlayer/$last_player
+
+# "black" or "white" - whose move it was during the previous round.
+
+# [input] boolean win/$win
+
+# true if the player who made a move during the previous round won, false otherwise.
+
+# [output] a string
+
+# Return "white" if white is to move on the next round, and "black" otherwise.
+
+# Example
+# For lastPlayer = "black" and win = false, the output should be "white".
+
+# For lastPlayer = "white" and win = true, the output should be "white".
+
+def whose_move(last_player, win):
+    ... # Your Move...
+    if last_player == "black" and win == False:
+        return "white"
+    elif last_player == "black" and win == True:
+        return "black"
+    elif last_player == "white" and win == False:
+        return "black"
+    elif last_player == "white" and win == True:
+        return "white"
