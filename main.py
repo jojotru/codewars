f4bc1a62f01de4d75d50e4fd113a97f67d348e96
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-10-28 22:56:15 -0400

    Challenge 10/28

--- main.py
+++ main.py
@@ -809,3 +809,53 @@ class Person:
 
 def merge_arrays(first, second): 
     return sorted(set(first + second))
+
+# It checks the char in the given string from the front with an iteration, in the iteration it does these steps:
+
+# There is a flag and it will be started from 1.
+# Check the current char in the iteration index.
+# If it is an alphabet character [a-zA-Z] and the flag value is equal to 1, then change this character to upper case.
+# If it is an alphabet character [a-zA-Z] and the flag value is equal to 0, then change this character to lower case.
+# Otherwise, if it is not an alphabet character, then set the flag value to 1.
+# If the current char is an alphabet character, do a boolean not operation to the flag.
+# After the iteration has done, return the fixed string that might have been changed in such iteration.
+
+# Examples
+# uglify_word("aaa") === "AaA"
+# uglify_word("AAA") === "AaA"
+# uglify_word("BbB") === "BbB"
+# uglify_word("aaa-bbb-ccc") === "AaA-BbB-CcC"
+# uglify_word("AaA-BbB-CcC") === "AaA-BbB-CcC"
+# uglify_word("eeee-ffff-gggg") === "EeEe-FfFf-GgGg"
+# uglify_word("EeEe-FfFf-GgGg") === "EeEe-FfFf-GgGg"
+# uglify_word("qwe123asdf456zxc") === "QwE123AsDf456ZxC"
+# uglify_word("Hello World") === "HeLlO WoRlD"
+
+def uglify_word(s):
+    flag = 1
+    ugly = []
+    for c in s:
+        ugly.append(c.upper() if flag else c.lower())
+        flag = not flag or not c.isalpha()
+    return ''.join(ugly)
+
+#OR
+
+def uglify_word(s: str) -> str:
+    flag = 1
+    result = []
+    for i in s:
+        if flag == 1 and i.isalpha():
+            result.append(i.upper())
+            flag = 0
+            continue
+        elif flag == 0 and i.isalpha():
+            result.append(i.lower())
+            flag = 1
+            continue
+        elif not i.isalpha():
+            flag = 1
+            result.append(i)
+            continue
+    return (''.join(result))
+
