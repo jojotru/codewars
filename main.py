03b8d2dc647ecd8fa7f48ac0c0909917c33f0949
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-11-07 23:42:30 -0500

    Challenge Nov/7

--- main.py
+++ main.py
@@ -972,3 +972,12 @@ def greet(language):
  return sum([abs(array[i] - array[i+1]) for i in range(len(array)-1)])
 
 
+# Given any number of boolean flags function should return true if and only if one of them is true while others are false. If function is called without arguments it should return false.
+
+#   only_one() == False
+#   only_one(True, False, False) == True
+#   only_one(True, False, False, True) == False
+#   only_one(False, False, False, False) == False  
+
+def only_one(*args):
+    return args.count(True)==1
