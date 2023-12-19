8e171f843f7612d4853aa6131cb1c060f4a593d6
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-12-19 01:04:02 -0500

    Challenge 12.19

--- main.py
+++ main.py
@@ -1097,12 +1097,27 @@ def same_case(a, b):
 
 #OR
 
-# def same_case(a, b): 
-#     if a.isalpha() and b.isalpha():
-#         if (a.islower() and b.islower()) or (a.isupper() and b.isupper()):
-#             return 1
-#         else:
-#             return 0
-#     else:
-#         return -1
+def same_case(a, b): 
+    if a.isalpha() and b.isalpha():
+        if (a.islower() and b.islower()) or (a.isupper() and b.isupper()):
+            return 1
+        else:
+            return 0
+    else:
+        return -1
+    
 
+# Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+# Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+# Examples:
+
+# * With `name` = "john"  => return "Hello, John!"
+# * With `name` = "aliCE" => return "Hello, Alice!"
+# * With `name` not given 
+#   or `name` = ""        => return "Hello, World!"
+
+
+def hello(name=""):
+    return "Hello, " + name.title()+"!" if len(name)>1  else "Hello, World!"
