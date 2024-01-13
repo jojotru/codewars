47c7bf0083c4a11f92360f3036c2122cd4b6deaa
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-01-12 23:43:24 -0500

    Add another completed challanege.

--- main.py
+++ main.py
@@ -1239,3 +1239,14 @@ def html_special_chars(data):
 
 def say_hello(name, city, state):
     return 'Hello, ' + ' '.join(name) + '! Welcome to ' + city + ', ' + state + '!'
+
+# Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+# Example
+# Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+def pipe_fix(nums):
+    return list(range(min(nums), max(nums) + 1))
+
+def pipe_fix(nums):
+	return list(range(nums[0], nums[-1] + 1))
