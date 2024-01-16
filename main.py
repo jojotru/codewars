82876f0941c14321315091593f4ca817683e3986
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-01-15 21:31:17 -0500

    Add a new completed challenge to the list.

--- main.py
+++ main.py
@@ -1274,3 +1274,12 @@ def contamination(text, char):
 
 def contamination(text, char):
   return char*len(text)
+
+#   Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+def combat(a, b):
+    c = a - b
+    if c > 0:
+        return c;
+    else:
+        return 0;
