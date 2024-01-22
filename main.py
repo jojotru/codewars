655780dae485c59e732cdf20fd9ade330a35bdb4
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-01-21 23:05:36 -0500

    Add a daily challenge. to the log

--- main.py
+++ main.py
@@ -1351,3 +1351,13 @@ def count_by(x, n):
         result = x * num
         arr.append(result)
     return arr
+
+
+# Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+# Example:
+# ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+
+def remove_every_other(my_list):
+    return my_list[::2]
