de306134e27557c061b9c0f72b6f230d3d355668
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-01-29 22:50:56 -0500

    Add another challenge to the log.

--- main.py
+++ main.py
@@ -1518,3 +1518,20 @@ def maxProduct (lst, n):
 
 def maps(a):
     return [2 * x for x in a]
+
+
+# Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+# The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+# Mind the input validation.
+
+# Example
+# { 6, 2, 1, 8, 10 } => 16
+# { 1, 1, 11, 2, 3 } => 6
+
+def sum_array(arr):
+    if arr is None or len(arr) <= 1:
+         return 0
+    else:
+         return sum(sorted(arr)[1:-1])
