81d53d944a1da2bd5477e8dab8fbf34faf937c3a
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-01-30 22:55:43 -0500

    Add a new challenge to the log

--- main.py
+++ main.py
@@ -1535,3 +1535,29 @@ def sum_array(arr):
          return 0
     else:
          return sum(sorted(arr)[1:-1])
+    
+
+def sum_array(arr):
+    if arr == None or len(arr) < 3:
+        return 0
+    return sum(arr) - max(arr) - min(arr)
+
+
+# You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+# As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+# For example(Input --> Output):
+
+10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ 1 --> [1]
+
+
+
+def monkey_count(n):
+    monkey = []
+    i = 0
+    while i < n:
+        i+=1
+        monkey.append(i)
+    return monkey
