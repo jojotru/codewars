033b215dfd331d87d7bff2e1677312f6ca98d89a
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-02-13 23:04:11 -0500

    Add daily challenge to log

--- main.py
+++ main.py
@@ -1590,3 +1590,17 @@ def powers_of_two(n):
 
 def powers_of_two(n):
     return [ 2**j for j in range(0,n+1)]
+
+# Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+# If no occurrences can be found, a count of 0 should be returned.
+
+# ("Hello", "o")  ==>  1
+# ("Hello", "l")  ==>  2
+# ("", "z")       ==>  0
+# str_count("Hello", 'o'); // returns 1
+# str_count("Hello", 'l'); // returns 2
+# str_count("", 'z'); // returns 0
+
+def str_count(strng, letter):
+    return strng.count(letter)
