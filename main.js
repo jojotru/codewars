abde65fadc8568610dd9c540c3d0b1b8e17fef71
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-02-29 23:41:27 -0500

    Added arrow buttons for movement

--- main.js
+++ main.js
@@ -49,3 +49,24 @@ function invert(array) {
 const stringToNumber = function(str){
   return Number(str);
 }
+
+
+// Create a function that always returns True/true for every item in a given list.
+// However, if an element is the word 'flick', switch to always returning the opposite boolean value.
+
+// Examples
+// ['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+
+// ['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+
+// ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+
+function flickSwitch(arr){
+  let on = true;
+  return arr.map((i) => {
+    if (i === "flick") {
+      on = !on;
+    }
+    return on;
+  });
+}
