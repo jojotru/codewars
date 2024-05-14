d358332a9e7e5392d363d15850f668f4dd6708dd
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-05-13 22:27:35 -0400

    Added new challenge to the log

--- main.js
+++ main.js
@@ -131,3 +131,18 @@ var cubeChecker = function (volume, side){
   }
   return false
 };
+
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+
+def paperwork(n, m):
+    if m<0 or n<0:
+        return 0
+    else:
+        return n*m
